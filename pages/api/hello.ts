06054ab26236aab028488a777047b9ef617c4b2e
Imamuddin AM <xeamam.hsu@gmail.com>
2025-07-26 08:40:47 +0700

    perbaikan keamanan + update index

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string
-}
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Atur header CORS
+  res.setHeader('Access-Control-Allow-Origin', 'https://web-skripsi-six.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  // Handle preflight (OPTIONS) request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  res.status(200).json({ message: 'Halo dari API Next.js!' });
 }
